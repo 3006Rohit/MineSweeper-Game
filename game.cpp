464ad3a80ce4d4331a46507e4395a41ad318b7bf
3006Rohit <125448661+3006Rohit@users.noreply.github.com>
2025-10-19 23:42:39 +0530

    updated_game.cpp

--- game.cpp
+++ game.cpp
@@ -1,163 +1,173 @@
-#include <bits/stdc++.h>
-#include <thread>
-#include <chrono>
-using namespace std;
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
 
-class Help {
-public:
-    void game() {
-        cout << "###########    ###########    #         #    ###########" << endl;
-        cout << "#          #    #         #    # #     # #    #          " << endl;
-        cout << "#          #    #         #    #  #   #  #    #          " << endl;
-        cout << "#          #    #         #    #   # #   #    #          " << endl;
-        cout << "#     #####    ###########    #    #    #    ######     " << endl;
-        cout << "#         #    #         #    #    #    #    #          " << endl;
-        cout << "#         #    #         #    #    #    #    #          " << endl;
-        cout << "#         #    #         #    #    #    #    #          " << endl;
-        cout << "###########    #         #    #    #    #    ###########" << endl;
-    }
+// Global variables
+int ROWS, COLS, MINES;
+std::vector<std::vector<char>> mineBoard;
+std::vector<std::vector<char>> playerBoard;
+bool gameOver;
+int movesLeft;
 
-    void start() {
-        cout << "###########    ###########    ###########    ###########    ###########" << endl;
-        cout << "#          #    #     #     #    #         #    #         #    #     # " << endl;
-        cout << "#          #    #     #     #    #         #    #         #    #     # " << endl;
-        cout << "#          #    #     #     #    #         #    #         #    #     # " << endl;
-        cout << "###########    #     #     #    ###########    ###########    #     # " << endl;
-        cout << "          #    #     #     #    #         #    #      #     #    #     " << endl;
-        cout << "          #    #     #     #    #         #    #       #    #    #     " << endl;
-        cout << "          #    #     #     #    #         #    #        #   #    #     " << endl;
-        cout << "###########    #     #     #    #         #    #         #   #    #    " << endl;
-    }
+// Function to check if coordinates are within the board
+bool isValid(int r, int c) {
+    return (r >= 0) && (r < ROWS) && (c >= 0) && (c < COLS);
+}
 
-    void over() {
-        cout << "###########    #         #    ###########    ###########" << endl;
-        cout << "#         #    #         #    #          #    #         #" << endl;
-        cout << "#         #    #         #    #          #    #         #" << endl;
-        cout << "#         #    #         #    #          #    #         #" << endl;
-        cout << "#         #    #       #      ###########    ###########" << endl;
-        cout << "#         #    #     #        #          #    #      #  " << endl;
-        cout << "#         #    #   #          #          #    #       # " << endl;
-        cout << "#         #    # * *          #          #    #        #" << endl;
-        cout << "###########    #     #        ###########    #         #" << endl;
-    }
+// Function to calculate adjacent mines for each cell
+void calculateAdjacentMines() {
+    for (int r = 0; r < ROWS; ++r) {
+        for (int c = 0; c < COLS; ++c) {
+            if (mineBoard[r][c] == '*') continue;
 
-    void win() {
-        cout << "#     #    #    #     #" << endl;
-        cout << "#     #    #    ##    #" << endl;
-        cout << "#     #    #    # #   #" << endl;
-        cout << "#  #  #    #    #  #  #" << endl;
-        cout << "# # # #    #    #   # #" << endl;
-        cout << "##   ##    #    #    ##" << endl;
-        cout << "#     #    #    #     #" << endl;
-    }
-
-    void lost() {
-        cout << "#         #######    #######    #######" << endl;
-        cout << "#         #     #    #          #   #" << endl;
-        cout << "#         #     #    #          #   #" << endl;
-        cout << "#         #     #    #######    #   #" << endl;
-        cout << "#         #     #          #    #   #" << endl;
-        cout << "#         #     #          #    #   #" << endl;
-        cout << "#######   #######    #######    #   #" << endl;
-    }
-};
-
-int main() {
-    srand(time(0));
-    Help ph;
-    ph.game();
-    this_thread::sleep_for(chrono::milliseconds(500));
-    cout << "\n\n\n";
-    ph.start();
-    this_thread::sleep_for(chrono::seconds(1));
-
-    int n;
-    cout << "Enter grid size: ";
-    cin >> n;
-    vector<vector<int>> a(n, vector<int>(n, 0));
-
-    cout << "Enter the number of bombs: ";
-    int b; cin >> b;
-
-    int c = 0;
-    int k = n - 1;
-    while (c != b) {
-        int r = rand() % n;
-        int l = rand() % n;
-        if (a[r][l] >= 100) continue; // prevent double bombs
-        a[r][l] = 100;
-
-        // Add numbers around bomb
-        for (int dr = -1; dr <= 1; dr++) {
-            for (int dc = -1; dc <= 1; dc++) {
-                if (dr == 0 && dc == 0) continue;
-                int nr = r + dr, nc = l + dc;
-                if (nr >= 0 && nr < n && nc >= 0 && nc < n && a[nr][nc] < 100) {
-                    a[nr][nc]++;
+            int mineCount = 0;
+            for (int dr = -1; dr <= 1; ++dr) {
+                for (int dc = -1; dc <= 1; ++dc) {
+                    if (dr == 0 && dc == 0) continue;
+                    int nr = r + dr;
+                    int nc = c + dc;
+                    if (isValid(nr, nc) && mineBoard[nr][nc] == '*') {
+                        mineCount++;
+                    }
                 }
             }
+            mineBoard[r][c] = mineCount + '0';
         }
-        c++;
+    }
+}
+
+// Function to set up the game boards and variables
+void setupGame() {
+    // Customizable grid (5x5 to 20x20)
+    do {
+        std::cout << "Enter number of rows (5-20): ";
+        std::cin >> ROWS;
+    } while (ROWS < 5 || ROWS > 20);
+
+    do {
+        std::cout << "Enter number of columns (5-20): ";
+        std::cin >> COLS;
+    } while (COLS < 5 || COLS > 20);
+
+    int maxMines = ROWS * COLS / 4; // Max 25% mines
+    do {
+        std::cout << "Enter number of mines (1-" << maxMines << "): ";
+        std::cin >> MINES;
+    } while (MINES < 1 || MINES > maxMines);
+
+
+    mineBoard.assign(ROWS, std::vector<char>(COLS, '0'));
+    playerBoard.assign(ROWS, std::vector<char>(COLS, '.'));
+    gameOver = false;
+    movesLeft = ROWS * COLS - MINES;
+
+    // Place mines randomly
+    srand(time(0));
+    int minesPlaced = 0;
+    while (minesPlaced < MINES) {
+        int r = rand() % ROWS;
+        int c = rand() % COLS;
+        if (mineBoard[r][c] != '*') {
+            mineBoard[r][c] = '*';
+            minesPlaced++;
+        }
     }
 
-    vector<vector<string>> na(n, vector<string>(n, "|__|"));
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) cout << setw(5) << na[i][j];
-        cout << endl;
+    // Calculate the numbers for non-mine cells
+    calculateAdjacentMines();
+}
+
+// Function to print the player's board
+void printBoard() {
+    system("cls"); // For Windows, use "clear" for Linux/Mac
+    std::cout << "   ";
+    for (int c = 0; c < COLS; ++c) std::cout << c << (c < 10 ? "  " : " ");
+    std::cout << "\n";
+
+    for (int r = 0; r < ROWS; ++r) {
+        std::cout << r << (r < 10 ? "  " : " ");
+        for (int c = 0; c < COLS; ++c) {
+            std::cout << playerBoard[r][c] << "  ";
+        }
+        std::cout << "\n";
+    }
+    std::cout << "\nMines left: " << MINES << "\n";
+}
+
+// Recursive function to uncover cells
+void uncoverCell(int r, int c) {
+    if (!isValid(r, c) || playerBoard[r][c] != '.') return;
+    
+    // Uncover the cell
+    char actualValue = mineBoard[r][c];
+    playerBoard[r][c] = actualValue;
+    
+    if (actualValue != '*') {
+         movesLeft--;
     }
 
-    int m = 0, point = 0;
-    while (m != n * n) {
-        int y, z, ch;
-        cout << "Enter row: "; cin >> y; y--;
-        cout << "Enter column: "; cin >> z; z--;
-        cout << "Enter choice 1-play 2-flag 3-doubt 4-exit: "; cin >> ch;
+    // If it's an empty cell ('0'), uncover its neighbors
+    if (actualValue == '0') {
+        for (int dr = -1; dr <= 1; ++dr) {
+            for (int dc = -1; dc <= 1; ++dc) {
+                if (dr == 0 && dc == 0) continue;
+                uncoverCell(r + dr, c + dc);
+            }
+        }
+    }
+}
 
-        switch (ch) {
-            case 1:
-                if (a[y][z] >= 100) {
-                    ph.game();
-                    this_thread::sleep_for(chrono::milliseconds(200));
-                    cout << endl << endl;
-                    ph.over();
-                    this_thread::sleep_for(chrono::seconds(1));
-                    goto endgame;
-                } else {
-                    na[y][z] = " " + to_string(a[y][z]) + " ";
+// Main function to handle user input and the game loop
+void playGame() {
+    setupGame();
+
+    while (!gameOver) {
+        printBoard();
+        std::cout << "Enter your move (e.g., 'p 2 3' to play, 'f 2 3' to flag): ";
+        char action;
+        int r, c;
+        std::cin >> action >> r >> c;
+
+        if (!isValid(r, c)) {
+            std::cout << "Invalid coordinates! Try again.\n";
+            continue;
+        }
+
+        if (action == 'p' || action == 'P') { // Play a move
+            if (mineBoard[r][c] == '*') {
+                gameOver = true;
+                // Reveal all mines
+                for(int i=0; i<ROWS; ++i) {
+                    for(int j=0; j<COLS; ++j) {
+                        if(mineBoard[i][j] == '*') playerBoard[i][j] = '*';
+                    }
                 }
-                break;
-            case 2:
-                na[y][z] = "|> ";
-                if (a[y][z] >= 100) point++;
-                break;
-            case 3:
-                na[y][z] = "? ";
-                break;
-            case 4:
-                ph.game();
-                this_thread::sleep_for(chrono::milliseconds(200));
-                cout << endl << endl;
-                ph.over();
-                this_thread::sleep_for(chrono::seconds(1));
-                goto endgame;
+                printBoard();
+                std::cout << "\nBOOM! You hit a mine. GAME OVER!\n";
+            } else {
+                uncoverCell(r, c);
+            }
+        } else if (action == 'f' || action == 'F') { // Flag a cell
+            if (playerBoard[r][c] == '.') {
+                playerBoard[r][c] = 'F';
+            } else if (playerBoard[r][c] == 'F') {
+                playerBoard[r][c] = '.'; // Unflag
+            }
+        } else {
+            std::cout << "Invalid action! Use 'p' to play or 'f' to flag.\n";
         }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) cout << setw(5) << na[i][j];
-            cout << endl;
+        
+        // Check for win condition
+        if (movesLeft == 0 && !gameOver) {
+            gameOver = true;
+            printBoard();
+            std::cout << "\nCongratulations! You have cleared the minefield! YOU WIN!\n";
         }
-        m++;
     }
+}
 
-endgame:
-    cout << endl << endl;
-    if (point >= b) {
-        this_thread::sleep_for(chrono::milliseconds(500));
-        ph.win();
-    } else {
-        this_thread::sleep_for(chrono::milliseconds(500));
-        ph.lost();
-    }
-    cout << "Points: " << point << endl;
+int main() {
+    playGame();
     return 0;
 }
